c53aedf04ea1055347420fbae87f95d2f2e08870
Alex Tilgreen Mogensen <alext@itu.dk>
2025-11-09 16:50:41 +0100

    node init

--- 04/node/node.go
+++ 04/node/node.go
@@ -4,20 +4,39 @@ import (
 	"flag"
 	"fmt"
 	"strings"
+	"sync"
 )
 
+type Node struct {
+	id           int
+	addr         string
+	peers        []string
+	timestamp    int64
+	requesting   bool
+	reqTimestamp int64
+	deferred     map[int]bool
+	mu           sync.Mutex
+}
+
 func main() {
+
 	id := flag.Int("id", 0, "node id")
 	port := flag.String("port", ":5000", "listen port")
-	peers := flag.String("peers", "", "comma-separated peer addresses")
-	flag.Parse()
+	p := flag.String("peers", "", "comma-separated peer addresses")
 
-	peerList := []string{}
-	if *peers != "" {
-		peerList = strings.Split(*peers, ",")
+	flag.Parse()
+	peers := strings.Split(*p, ",")
+
+	n := &Node{
+		id:           *id,
+		addr:         *port,
+		peers:        peers,
+		timestamp:    0,
+		requesting:   false,
+		reqTimestamp: 0,
+		deferred:     make(map[int]bool),
+		mu:           sync.Mutex{},
 	}
 
-	fmt.Println("ID:", *id)
-	fmt.Println("Port:", *port)
-	fmt.Println("Peers:", peerList)
+	fmt.Printf("node init: \n%+v\n", n)
 }
